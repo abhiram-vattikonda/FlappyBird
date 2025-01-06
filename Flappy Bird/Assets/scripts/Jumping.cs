283cfc36af5eba5c653eb2750afd926d04c62a92
abhee238 <abhiram.vattikonda@gmail.com>
2025-01-06 09:48:57 +0530

    made changes

--- Flappy Bird/Assets/scripts/Jumping.cs
+++ Flappy Bird/Assets/scripts/Jumping.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Jumping : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInputActions playerInputActions;
+    private Vector3 gravity = new Vector3 (0f, -9.8f, 0f);
+    private Vector3 upwardForce = new Vector3(0f, 20f, 0f);
+    private Rigidbody2D rb;
+
+
+    private void Start()
     {
-        
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Enable();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position += gravity * Time.deltaTime;
+
+        if (playerInputActions.Player.Jump.IsPressed())
+        {
+            rb.AddForce(upwardForce * Time.deltaTime);
+        }
     }
 }
